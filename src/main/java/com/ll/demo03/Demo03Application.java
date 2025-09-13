5cc9a84e18cdb951df9a2653344572fe7eddc19d
reas0njeong <seahorse2001@naver.com>
2025-09-13 15:17:28 +0900

    ìchap 06~07-05

--- src/main/java/com/ll/demo03/Demo03Application.java
+++ src/main/java/com/ll/demo03/Demo03Application.java
@@ -2,8 +2,10 @@ package com.ll.demo03;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Demo03Application {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/ll/demo03/domain/member/member/service/MemberService.java
@@ -0,0 +1,44 @@
+package com.ll.demo03.domain.member.member.service;
+
+
+import com.ll.demo03.domain.member.member.entity.Member;
+import com.ll.demo03.domain.member.member.repository.MemberRepository;
+import com.ll.demo03.global.rsData.RsData;
+import com.ll.demo03.global.exception.GlobalException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    private Optional<Member> findByUsername(String username) {
+        return memberRepository.findByUsername(username);
+    }
+
+    @Transactional
+    public RsData<Member> join(String username, String password, String nickname) {
+        findByUsername(username).ifPresent(ignored -> {
+            throw new GlobalException("400-1", "%s(은)는 이미 존재하는 아이디입니다.".formatted(username));
+        });
+
+        Member member = Member.builder()
+                .username(username)
+                .password(password)
+                .nickname(nickname)
+                .build();
+
+        memberRepository.save(member);
+
+        return RsData.of("회원가입이 완료되었습니다.", member);
+    }
+
+    public Member getReferenceById(long id) {
+        return memberRepository.getReferenceById(id);
+    }
+}
