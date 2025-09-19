3de095e8efdde39ba1a95e94d86ba0d31c9615de
reas0njeong <seahorse2001@naver.com>
2025-09-20 00:22:16 +0900

    chap 8

---
+++ src/main/java/com/ll/demo03/domain/member/member/controller/ApiV1MemberController.java
@@ -0,0 +1,51 @@
+package com.ll.demo03.domain.member.member.controller;
+
+import com.ll.demo03.domain.member.member.entity.Member;
+import com.ll.demo03.domain.member.member.service.MemberService;
+import com.ll.demo03.global.rsData.RsData;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/members")
+@RequiredArgsConstructor
+@Slf4j
+public class ApiV1MemberController {
+    private final MemberService memberService;
+
+    @AllArgsConstructor
+    @Getter
+    public static class MemberJoinReqBody {
+        @NotBlank
+        private String username;
+        @NotBlank
+        private String password;
+        @NotBlank
+        private String nickname;
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class MemberJoinResBody {
+        Member item;
+    }
+
+    @PostMapping("")
+    public RsData<MemberJoinResBody> join(@RequestBody @Valid MemberJoinReqBody reBody) {
+        RsData<Member> joinRs = memberService.join(reBody.username, reBody.password, reBody.nickname);
+
+        return joinRs.newDataOf(
+                new MemberJoinResBody(joinRs.getData())
+        );
+    }
+
+}
+
--- src/main/java/com/ll/demo03/global/rsData/RsData.java
+++ src/main/java/com/ll/demo03/global/rsData/RsData.java
@@ -34,7 +34,7 @@ public class RsData<T> {
     }
 
     public static <T> RsData<T> of(String msg, T data) {
-        return of("200-1", msg, data);
+        return of("205-1", msg, data);
     }
 
     public static <T> RsData<T> of(String resultCode, String msg) {
