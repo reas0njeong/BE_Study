3228285cddff4232e7763824b7e2d89c2e9228d9
reas0njeong <seahorse2001@naver.com>
2025-09-20 17:35:39 +0900

    ììchap 10

--- src/main/java/com/ll/demo03/domain/member/member/controller/ApiV1MemberController.java
+++ src/main/java/com/ll/demo03/domain/member/member/controller/ApiV1MemberController.java
@@ -3,7 +3,10 @@ package com.ll.demo03.domain.member.member.controller;
 import com.ll.demo03.domain.member.member.dto.MemberDto;
 import com.ll.demo03.domain.member.member.entity.Member;
 import com.ll.demo03.domain.member.member.service.MemberService;
+import com.ll.demo03.global.exception.GlobalException;
+import com.ll.demo03.global.rq.Rq;
 import com.ll.demo03.global.rsData.RsData;
+import com.ll.demo03.standard.dto.Empty;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
@@ -11,10 +14,7 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/members")
@@ -23,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class ApiV1MemberController {
     private final MemberService memberService;
+    private final Rq rq;
 
     @AllArgsConstructor
     @Getter
@@ -55,5 +56,53 @@ public class ApiV1MemberController {
         );
     }
 
+
+    @AllArgsConstructor
+    @Getter
+    public static class MemberLoginReqBody {
+        @NotBlank
+        private String username;
+        @NotBlank
+        private String password;
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class MemberLoginRespBody {
+        MemberDto item;
+    }
+
+    @PostMapping("/login")
+    @Transactional
+    public RsData<MemberLoginRespBody> login(@RequestBody @Valid MemberLoginReqBody reqBody) {
+        Member member = memberService
+                .findByUsername(reqBody.username)
+                .orElseThrow(() -> new GlobalException("401-1", "해당 회원이 존재하지 않습니다."));
+
+        if (!member.getPassword().equals(reqBody.password)) {
+            throw new GlobalException("401-2", "비밀번호가 일치하지 않습니다.");
+        }
+
+        rq.setCookie("actorUsername", member.getUsername());
+        rq.setCookie("actorPassword", member.getPassword());
+
+        return RsData.of(
+                "200-1",
+                "로그인 되었습니다.",
+                new MemberLoginRespBody(
+                        new MemberDto(member)
+                )
+        );
+    }
+
+
+    @DeleteMapping("/logout")
+    @Transactional
+    public RsData<Empty> logout() {
+        rq.removeCookie("actorUsername");
+        rq.removeCookie("actorPassword");
+
+        return RsData.OK;
+    }
 }
 
--- src/main/java/com/ll/demo03/global/rq/Rq.java
+++ src/main/java/com/ll/demo03/global/rq/Rq.java
@@ -4,11 +4,14 @@ import com.ll.demo03.domain.member.member.entity.Member;
 import com.ll.demo03.domain.member.member.service.MemberService;
 import com.ll.demo03.global.exception.GlobalException;
 import com.ll.demo03.standard.dto.util.Ut;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
+
+import java.util.Arrays;
 
 @Component
 @RequestScope
@@ -22,8 +25,20 @@ public class Rq {
     public Member getMember() {
         if ( member != null ) return member;
 
-        String actorUsername = req.getParameter("actorUsername");
-        String actorPassword = req.getParameter("actorPassword");
+        getCookieValue("actorUsername", null);
+
+        String actorUsername = getCookieValue("actorUsername", null);
+        String actorPassword = getCookieValue("actorPassword", null);
+
+        if (actorUsername == null || actorPassword == null) {
+            String authorization = req.getHeader("Authorization");
+            if (authorization != null) {
+                authorization = authorization.substring("bearer ".length());
+                String[] authorizationBits = authorization.split(" ", 2);
+                actorUsername = authorizationBits[0];
+                actorPassword = authorizationBits.length == 2 ? authorizationBits[1] : null;
+            }
+        }
 
         if ( Ut.str.isBlank(actorUsername)) throw new GlobalException("401-1", "인증정보(아이디)를 입력해주세요");
         if ( Ut.str.isBlank(actorPassword)) throw new GlobalException("401-1", "인증정보(비밀번호)를 입력해주세요");
@@ -43,4 +58,28 @@ public class Rq {
     public void setStatusCode(int statusCode) {
         resp.setStatus(statusCode);
     }
+
+    private String getCookieValue(String cookieName, String defaultValue) {
+        return Arrays.stream(req.getCookies())
+                .filter(cookie -> cookie.getName().equals(cookieName))
+                .findFirst()
+                .map(Cookie::getValue)
+                .orElse(defaultValue);
+
+    }
+
+    public void removeCookie(String cookieName) {
+        Cookie cookie = new Cookie(cookieName, null);
+        cookie.setMaxAge(0);
+        cookie.setPath("/");
+        resp.addCookie(cookie);
+    }
+
+    public void setCookie(String actorUsername, String username) {
+        Cookie cookie = new Cookie(actorUsername, username);
+        cookie.setMaxAge(60 * 60 * 24 * 365);
+        cookie.setPath("/");
+        resp.addCookie(cookie);
+    }
+
 }
